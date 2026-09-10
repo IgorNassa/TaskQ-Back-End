4d1b3f550393b3cddb826d146262917ca648380a
IgorNassa <igoral.pessoal@gmail.com>
2026-09-10 20:19:09 -0300

    Initial commit

---
+++ Desktop/tasksq-back-end/src/main/java/uniamerica/tasksq_back_end/TaskqBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.uniamerica.taskq_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskqBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskqBackEndApplication.class, args);
+	}
+
+}
---
+++ Desktop/tasksq-back-end/src/test/java/uniamerica/tasksq_back_end/TasksqBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package uniamerica.tasksq_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksqBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
