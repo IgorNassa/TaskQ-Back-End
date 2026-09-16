6dcf814dd888b17e62d35b4d386f1089843b2513
Mourvan Maes <mourvanhmaes@gmail.com>
2026-09-15 23:03:27 -0300

    feature: dependencia swagger

--- Desktop/tasksq-back-end/src/main/resources/db/migration/V1__create_table.sql
+++ Desktop/tasksq-back-end/src/main/resources/db/migration/V1__create_table.sql
@@ -1,15 +1,14 @@
 CREATE TABLE tb_task (
-                         id BIGSERIAL PRIMARY KEY,
-                         title VARCHAR(255) NOT NULL,
-                         description TEXT NOT NULL,
-                         status VARCHAR(50) NOT NULL,
-                         priority VARCHAR(50) NOT NULL,
-                         assignee_id BIGINT NOT NULL,
-                         creator_id BIGINT NOT NULL,
-                         dead_line DATE NOT NULL,
-                         created_at TIMESTAMP NOT NULL,
-                         updated_at TIMESTAMP NOT NULL,
-                         completed_at TIMESTAMP,
-                         created_by BIGINT,
-                         updated_by BIGINT
-);
+     id BIGSERIAL PRIMARY KEY,
+     title VARCHAR(255) NOT NULL,
+     description TEXT NOT NULL,
+     status VARCHAR(50) NOT NULL,
+     priority VARCHAR(50) NOT NULL,
+     assignee_id BIGINT NOT NULL,
+     creator_id BIGINT NOT NULL,
+     dead_line DATE NOT NULL,
+     created_at TIMESTAMP NOT NULL,
+     updated_at TIMESTAMP NOT NULL,
+     completed_at TIMESTAMP,
+     created_by BIGINT,
+     updated_by BIGINT
