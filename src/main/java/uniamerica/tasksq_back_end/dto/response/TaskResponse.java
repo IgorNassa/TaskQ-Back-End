a4fd79863df6127521a5014122358081f4aea44a
Mourvan Maes <mourvanhmaes@gmail.com>
2026-09-17 21:49:08 -0300

    fix: response em record

--- src/main/java/uniamerica/tasksq_back_end/dto/response/TaskResponse.java
+++ src/main/java/uniamerica/tasksq_back_end/dto/response/TaskResponse.java
@@ -1,29 +1,22 @@
 package uniamerica.tasksq_back_end.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import uniamerica.tasksq_back_end.entity.enums.TaskPriority;
 import uniamerica.tasksq_back_end.entity.enums.TaskStatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class TaskResponse {
-    private Long id;
-    private String title;
-    private String description;
-    private TaskStatus status;
-    private TaskPriority priority;
-    private Long assigneeId;
-    private Long creatorId;
-    private LocalDate deadLine;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private LocalDateTime completedAt;
+public record TaskResponse(
+        Long id,
+        String title,
+        String description,
+        TaskStatus status,
+        TaskPriority priority,
+        Long assigneeId,
+        Long creatorId,
+        LocalDate deadLine,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        LocalDateTime completedAt
+) {
 }
